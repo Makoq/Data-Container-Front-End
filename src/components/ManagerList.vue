c1e8cd978ed2f031aa8a21c1b59e8d35136079ae
limingyuan123 <1484156313@qq.com>
2020-12-07 17:05:29 +0800

    优化样式，v-if

--- src/components/ManagerList.vue
+++ src/components/ManagerList.vue
@@ -1,39 +1,5 @@
 <template>
   <div>
-    <!-- <el-table
-        :data="listData"
-        style="width: 100%">
-        <el-table-column
-          label="name"
-          width="180">
-          <template slot-scope="scope">
-            <i class="el-icon-time"></i>
-            <span style="margin-left: 10px">{{ scope.row.date }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column
-          label="Tags"
-          width="180">
-          <template slot-scope="scope">
-            <el-popover trigger="hover" placement="top">
-              <p>name: {{ scope.row.name }}</p>
-            </el-popover>
-          </template>
-        </el-table-column>
-        <el-table-column label="Operate">
-          <template slot-scope="scope">
-            <el-button
-              size="mini"
-              :style="selectedWorkspace(scope.$index, scope.row)"
-              @click="handleSelect(scope.$index, scope.row)">Select</el-button>
-            <el-button
-              size="mini"
-              type="danger"
-              @click="handleDelete(scope.$index, scope.row)">Delete</el-button>
-          </template>
-        </el-table-column>
-      </el-table>  -->
-
     <div
       v-for="(item, index) in listData"
       :key="index"
@@ -42,38 +8,19 @@
       <div class="content_h32 flexRow noPadding" style="margin-top: 10px;">
         <div class="content_w68 flexJustStart" style="margin-left: 1%;">
           <div class="flexColCenter w100">
-            <a class="w100" :href="'/item/hub/' + item.Uid">
-              <h4 class="itemTitle">{{ item.name }}</h4></a
-            >
+            <a class="w100">
+              <h4 class="itemTitle">{{ item.name }}</h4>
+            </a>
           </div>
         </div>
         <div
           class="content_w32 flexJustAround flexColCenter buttonZone"
           style="text-align: right; margin: 0 auto 0 auto;"
         >
-          <el-tooltip
-            popper-class="fontsize-15"
-            effect="light"
-            :content="item.status"
-            placement="left"
-          >
-            <i
-              v-if="item.status == 'Public'"
-              class="el-icon-unlock public fontsize-20 pull-right"
-            ></i>
-            <i
-              v-if="item.status == 'Discoverable'"
-              class="el-icon-unlock discoverable fontsize-20 pull-right"
-            ></i>
-            <i
-              v-if="item.status == 'Private'"
-              class="el-icon-lock private fontsize-20 pull-right"
-            ></i>
-          </el-tooltip>
-          <div style="margin-right:25px" class="select" :id="gernerateId(index)">
+          <div v-if="active!=index" style="margin-right:25px" class="select" :id="gernerateId(index)">
             <el-button
               type="info"
-              class="selectBtn"              
+              class="selectBtn"
               style="display:inline-block;padding:5px;margin-right: 5px;"
               @click="handleSelect(index,item)"
               icon="el-icon-star-off"
@@ -81,7 +28,7 @@
             ></el-button>
             <p style="display:inline-block">Select</p>
           </div>
-          <div style="margin-right:25px;display:none" class="selectOk" :id="gernerateIdOk(index)">
+          <div v-if="active==index" style="margin-right:25px" class="selectOk" :id="gernerateIdOk(index)">
             <el-button
               type="primary"
               class="selectBtn"
@@ -102,7 +49,7 @@
             </div>
           </div>
           <div
-            class="btn btn-danger btn-round btn-noShadow waves-effect waves-light flexCenter"
+            class="btn btn-danger btn-round btn-noShadow waves-effect waves-light flexCenter"  v-if="index!=0"
           >
             <i class="fa fa-trash-o"></i>
             <p>Delete</p>
@@ -153,7 +100,9 @@ import $ from "jquery";
 export default {
   props: ["listData"],
   data() {
-    return {};
+    return {
+      active:0,
+    };
   },
   mounted() {},
   methods: {
@@ -166,13 +115,16 @@ export default {
     handleSelect(index, row) {
       //  TODO: 选择工作空间
       console.log(index, row);
+      this.active = index;
       //所有的先初始化为btn展示
-      $(".selectOk").hide();
-      $(".select").show();
+      // $(".selectOk").hide();
+      // $(".select").show();
 
       //具体的btn显示与隐藏操作
-      $("#selectBtn"+index).hide();
-      $("#selectBtnOk" + index).show();
+      // $("#selectBtn"+index).hide();
+      // $("#selectBtnOk" + index).show();
+
+
     },
     handleDelete(index, row) {
       console.log(index, row);
